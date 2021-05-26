d91224ad71e1446457524f1dbcaf0ad0ce9ef0b5
Bruno Sajermann <contato@sajermann.com>
2021-05-26 20:58:49 -0300

    Day Three Complete

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/core';
 import { Text, SafeAreaView, StyleSheet, Image, TouchableOpacity, TextInput, View, KeyboardAvoidingView, Platform } from 'react-native';
 import { Button } from '../components/Button';
 
@@ -6,6 +7,11 @@ import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 export function Confirmation(){
+	const navigation = useNavigation();
+	function handleMoveOn(){
+		navigation.navigate('PlantSelect');
+	}
+
 	return(
     <SafeAreaView style={styles.container}>
 			<View style={styles.content}>
@@ -19,7 +25,7 @@ export function Confirmation(){
 					Agora vamos começar a cuidar das suas plantinhas com muito cuidado
 				</Text>
 				<View style={styles.footer}>
-					<Button title="Começar" />
+					<Button title="Começar" onPress={handleMoveOn} />
 				</View>
 			</View>
     </SafeAreaView>
